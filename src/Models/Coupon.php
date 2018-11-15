b2da1c81de5e57a0dec56591f8dac61417a6447c
viviniko <viviniko.shop@gmail.com>
2018-11-15 15:20:08 +0800

    :trollface:

--- src/Models/Coupon.php
+++ src/Models/Coupon.php
@@ -8,7 +8,7 @@ class Coupon extends Model
 {
     protected $tableConfigKey = 'promotion.promotion_coupons_table';
 
-    protected $fillable = ['code', 'usage_limit', 'uses_per_user', 'used_num', 'total_amount', 'type', 'promotion_id'];
+    protected $fillable = ['code', 'usage_limit', 'uses_per_user', 'used_num', 'total_amount', 'type', 'promotion_id', 'status'];
 
     public function promotion()
     {
